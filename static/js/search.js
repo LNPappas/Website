107bd1d8c776bb4e46bd659994d6153350803d5f
Lauren Pappas <LPappas@gmail.com>
2019-10-21 16:02:07 -0700

    update

--- static/js/search.js
+++ static/js/search.js
@@ -1,6 +1,21 @@
 document.addEventListener('click', function(e) {
     id = e.target.id
+    id = id.split('tt').join('')
     console.log(id)
 
-    
+    const proxyurl = "https://whispering-wave-91848.herokuapp.com/"
+    const url = "https://themovieclips.p.rapidapi.com/trailers?imdb="+id+"&limit=3"
+
+    fetch(proxyurl+url, {
+        "method": "GET",
+        "headers": {
+            "x-rapidapi-host": "themovieclips.p.rapidapi.com",
+            "x-rapidapi-key": "80d1b10cb0mshe34b0eb53cf0a02p1fed07jsn893bd88913f3"
+        }
+    })
+    .then(response => response.document())
+    .then(contents => console.log(200))
+    .catch(err => {
+        console.log(err);
+    });
 });
